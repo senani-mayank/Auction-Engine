683f18264d9aee92153566e98c7c88578db51fd6
sharad <mjnsam123@gmail.com>
2018-04-10 16:14:59 +0530

    kth

--- code/lib/KTH_PRICE.js
+++ code/lib/KTH_PRICE.js
@@ -6,38 +6,33 @@
 var KTH_PRICEtimeoutInterval = 1000;
 
 
-/**Invkkoked when an english auction bid is places
- * @param {IN.AC.IIITB.KTH_PRICE.PlaceKTH_PRICEAuctionBid} placeBidTransaction
+/**Invkkoked when an Kth Price auction bid is places
+ * @param {IN.AC.IIITB.KTH_PRICE.PlaceKTH_PRICEBid} placeBidTransaction
  * @transaction
  */
 function onKTH_PRICEBidPlaced( placeBidTransaction ) {
-    console.log("onKTH_PRICEBidPlaced", placeBidTransaction);
+    
     var NS = "IN.AC.IIITB.KTH_PRICE";
     var bid = placeBidTransaction.bid;
     var bidder = bid.bidder;
     var auction = bid.auction;
-    var auctionItem = auction.auctionItem ;
+    var auctionItem = auction.auctionItem;
     var bidValue = bid.bidValue;
 
     if( auction.status == "CREATED" ){
-        console.log("Auction Has Not Started Yet..!");
-        return "Auction Has Not Started Yet..!";
+        throw new Error("Auction Has Not Started Yet..!");
     }
     else if( auction.status == "FINISHED" ){
-        console.log("This Auction is Over..!");
-        return "This Auction is Over..!";
+        throw new Error("This Auction is Over..!");
     }
 
     //check if auction should be closed
     var now = placeBidTransaction.timestamp;
     var timeoutTime = new Date( auction.auctionStartTime) ;
-    timeoutTime.setMinutes( timeoutTime.getMinutes() + 2 );
+    timeoutTime.setMinutes( timeoutTime.getMinutes() + 5 );
 
     if( ( !auction.lastBidTimestamp ) && ( now >= timeoutTime ) ){//no bid & times up
-        console.log("no bid placed and auction time is up, item unsold");
-      	console.log(now," -> ",timeoutTime );
-      	console.log("auction",auction);
-        return;
+        throw new Error("no bid placed and auction time is up, item unsold");
     }
     else {
 
@@ -45,28 +40,26 @@ function onKTH_PRICEBidPlaced( placeBidTransaction ) {
             auction.lastBidTimestamp =  placeBidTransaction.timestamp;
         }
 
-		console.log("andar aaya..!");
-        timeoutTime =  new Date( auction.lastBidTimestamp );
-        timeoutTime.setMinutes( timeoutTime.getMinutes() + 2 );
-        console.log("bas andar hi aaya..!");
+       // timeoutTime =  new Date( auction.lastBidTimestamp );
+        //timeoutTime.setMinutes( timeoutTime.getMinutes() + 2 );
 
         if( auction.lastBidTimestamp && ( now >= timeoutTime ) ){//if last bid was placed 2 minutes before
-            console.log("Time Out Has Occured, item is sold");
-            console.log(timeoutTime.getTime() , now);
-            return;
+            throw new Error("Time Out Has Occured, item is sold");
         }
         else{
             //if current bid is > maxbid till now
-          	console.log("lo jee else mai bhi aaya");
-            if(  ( !auction.currentMaxBid ) ||  ( auction.currentMaxBid.bidValue < bidValue ) ){
-                auction.currentMaxBid.bidValue = bidValue;
-                auction.lastBidTimestamp = placeBidTransaction.timestamp;
+            if(  ( !auction.currentMaxBid ) ||  ( auction.currentMaxBid.bidValue <= bidValue )||  ( auction.currentMaxBid.bidValue > bidValue ) ){
+
+                auction["currentMaxBid"] = bid;
+               // auction.lastBidTimestamp = placeBidTransaction.timestamp;
+                if( !auction.bids ){ // if bids array is not initialized
+                    auction.bids = [];
+                }
                 auction.bids.push( bid );
                 return updateAssets( auction );
             }
             else{
-                console.log("Your Bid Should Be Grater than Current Max Bid.!");
-                return "Your Bid Should Be Grater than Current Max Bid.";
+                throw new Error ("Your Bid Should Be Greater than Current Max Bid.");
             }
            
         }    
@@ -76,64 +69,63 @@ function onKTH_PRICEBidPlaced( placeBidTransaction ) {
     function updateAssets( auction, auctionItem ){
 
         return getAssetRegistry( NS + '.KTH_PRICE' )
-        .then(function ( KTH_PRICEAuctionRegistery ) {
+        .then(function ( KTH_PRICERegistery ) {
             // add the temp reading to the shipment
-            console.log("Auction Updated Successfully.!");
-            return KTH_PRICEAuctionRegistery.update( auction );
-        });
-        /*
-        .then(function() {
-            return getAssetRegistry( NS + '.EnglishAuctionItem' );
+            return KTH_PRICERegistery.update( auction );
         })
-        .then(function( englishAuctionItemRegistery ) {
-            // add the temp reading to the shipment
-            console.log("Bid Placed Successfully.!");
-            return englishAuctionItemRegistery.update(auctionItem);
-        });
-        */
-        
+        .then(function ( ) {//emt event about update asset
+
+            var factory = getFactory();
+            var bidPlaceEvent = factory.newEvent( NS , 'KTH_PRICEBidUpdate');
+            bidPlaceEvent.bidValue = auction.currentMaxBid.bidValue;
+            bidPlaceEvent.bid = auction.currentMaxBid;
+            bidPlaceEvent.bids = auction.bids;  
+            bidPlaceEvent.auction = auction;          
+            return emit( bidPlaceEvent );
+
+        });        
+
     }
 
-
 }
+
 
 /**Invoked start the auction
  * @param {IN.AC.IIITB.KTH_PRICE.StartKTH_PRICE} startAuction
  * @transaction
  */
 function onKTH_PRICEStart( startAuction ) {
-  
+
     var NS = "IN.AC.IIITB.KTH_PRICE";
+    //var factory = getFactory();
+    //var bidPlaceEvent = factory.newEvent( NS , 'testEvent');
+   // emit(bidPlaceEvent);
+   
     var auction = startAuction.auction;
 
     if( auction.status == "FINISHED" ){
-        console.log("Auction is ALready Over");
-        return "Auction is ALready Over...!";
+        throw new Error ( "Auction is ALready Over...!" );
     }
     else if( auction.status == "IN_PROGRESS" ){
-        console.log("Auction is ALready Running");
-        return "Auction is Already Running...!";
+        throw new Error ( "Auction is Already Running...!" );
     }
 
     auction.status = "IN_PROGRESS";
     auction.auctionStartTime = startAuction.timestamp;
-    //auction.auctionItem.auctionStartTime = startAuction.timestamp;//remove it later
     auction.auctionItem.status = "AUCTIONING";
 
     return  getAssetRegistry( NS + '.KTH_PRICEItem' )//update auctionItem status
-            .then(function ( KTH_PRICEAuctionItemRegistry ) {
-                console.log("Auction Item Updated Successfully.!");
-                return KTH_PRICEAuctionItemRegistry.update( auction.auctionItem );
+            .then(function ( KTH_PRICEItemRegistry ) {
+                return KTH_PRICEItemRegistry.update( auction.auctionItem );
             })
             .then(function(){
                 return getAssetRegistry( NS + '.KTH_PRICE' );
             })
-            .then(function( KTH_PRICEAuctionRegistry ){
-                console.log("Auction Updated Successfully.!");
-                return KTH_PRICEAuctionRegistry.update( auction );
-            });
+            .then(function( KTH_PRICERegistry ){
+                return KTH_PRICERegistry.update( auction );
+            });             ;
 
-}//end startEnglishAuction
+}//end startKTH_PRICE
 
 
 /**Invoked stop the auction
@@ -144,27 +136,51 @@ function stopKTH_PRICE( stopAuction ) {
   
     var NS = "IN.AC.IIITB.KTH_PRICE";
     var auction = stopAuction.auction;
+    var auctionItem = auction.auctionItem;    
 
     if( auction.status == "FINISHED" ){
-        console.log("Auction is ALready Over");
-        return "Auction is ALready Over...!";
+        throw new Error ( "Auction is ALready Over...!");
     }
     else if( auction.status == "CREATED" ){
-        console.log("Auction is Not Started Yet");
-        return "Auction is Not Started Yet...!";
+        throw new Error ( "Auction is Not Started Yet...!" );
     }
 
-    auction.status = "FINISHED";
-    auction.auctionEndTime = stopAuction.timestamp;
+    if( !auction.currentMaxBid ){
+        auctionItem.status = "UNSOLD"; 
+    }
+    else{
+        auctionItem.status = "SOLD"; 
+    }
 
-    return  getAssetRegistry( NS + '.KTH_PRICE' )//update auctionItem status
-            .then(function ( KTH_PRICEAuctionRegistry ) {
-                console.log("Auction Updated Successfully.!");
-                return KTH_PRICEAuctionRegistry.update( auction );
+    auction.status = "FINISHED";   
+    auction.auctionEndTime = stopAuction.timestamp;
+    var k=auction.k;
+    var n = auction.bids.legth;
+    auction.bids.sort();
+    auction.winnerBid = auction.bids[n-1];
+
+    return  getAssetRegistry( NS + '.KTH_PRICEItem' )//update auctionItem status
+            .then(function ( KTH_PRICEItemRegistry ) {
+                return KTH_PRICEItemRegistry.update( auctionItem );
+            })
+            .then(function(){
+                return getAssetRegistry( NS + '.KTH_PRICE' );
+            })
+           .then(function( KTH_PRICERegistry ){
+                return KTH_PRICERegistry.update( auction );
+            })
+            .then(function ( ) {//emt event about update asset
+
+                var factory = getFactory();
+                var stopAuctionEvent = factory.newEvent( NS , 'KTH_PRICEStopEvent');
+                stopAuctionEvent.auction = auction;
+                stopAuctionEvent.winnerBid = auction.bids[n-1];
+                return emit( stopAuctionEvent );
+    
             });
 
 
-}//end startEnglishAuction
+}//end startKTH_PRICE
 
 
 
@@ -180,28 +196,27 @@ function onItemSold( itemSold ) {
     var auctionItem = auction.auctionItem;
 
     if( auction.status == "CREATED" ){
-        console.log("Auction is not started yet..!");
-        return "Auction is not started yet..!";
+        throw new Error ( "Auction is not started yet..!" );
     }
     else if( auction.status == "IN_PROGRESS" ){
-        console.log("Auction is IN_PROGRESS");
-        return "Auction is IN_PROGRESS";
+        throw new Error ( "Auction is IN_PROGRESS" );
     }
 
-   // auction.status = "CLOSED";
-   // auction.auctionItem.auctionEndTime = itemSold.timestamp;
     auctionItem.status = "SOLD";
     auction.winnerBid = winnerBid;
+    var kthbid;
+    var k = auction.k; 
+    auction.winnerBid = auction.bids[n-1];
+       kthbid = auction.bids[n-k]; 
     return  getAssetRegistry( NS + '.KTH_PRICEItem' )//update auctionItem status
-            .then(function ( KTH_PRICEAuctionItemRegistry ) {
-                console.log("1");
-                return KTH_PRICEAuctionItemRegistry.update( auctionItem );
+            .then(function ( KTH_PRICEItemRegistry ) {
+                return KTH_PRICEItemRegistry.update( auctionItem );
             })
             .then(function(){
                 return getAssetRegistry( NS + '.KTH_PRICE' );
             })
-           .then(function( KTH_PRICEAuctionRegistry ){
-                console.log("2");
-                return KTH_PRICEAuctionRegistry.update( auction );
+           .then(function( KTH_PRICERegistry ){
+                return KTH_PRICERegistry.update( auction );
             });
+            
 }//end startKTH_PRICE
